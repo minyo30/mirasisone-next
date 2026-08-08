e7201cd6794c45a0e9ef6b76cc8fab21e20be054
minyo30 <mio.1016.3030@gmail.com>
2026-08-08 22:19:19 +0900

    Fix homepage CONTACT links to point to /contact

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,5 @@ import { StaticHtmlPage } from "@/components/StaticHtmlPage";
 import { topPage } from "@/content/top";
 
 export default function HomePage() {
-  return <StaticHtmlPage page={topPage} />;
+  return <StaticHtmlPage page={topPage} contactLinks />;
 }
